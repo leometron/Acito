cf97bfcc094496a0114106c4e053f3724644cdbc
satheeshraj <satheeshraj@ymail.com>
2015-07-22 12:23:17 +0530

    dashboard ui added

---
+++ client/views/admin/header/header.js
@@ -0,0 +1,19 @@
+Template.adminHeader.helpers({
+    'navBarList' : function(){
+        return [
+            {'title': 'Dashboard'},
+            {'title': 'Posts', 'subtitle': ['AddNew','Categories','Tags']},
+            {'title': 'LatestNews'},
+            {'title': 'investors'},
+            {'title': 'OurWorks'},
+            {'title': 'HomeSliders'},
+            {'title': 'Media'},
+            {'title': 'Pages'},
+            {'title': 'Settings'},
+            {'title': 'SEO'},
+            {'title': 'Types'},
+            {'title': 'collapseMenu'}
+        ]
+    }
+
+});
