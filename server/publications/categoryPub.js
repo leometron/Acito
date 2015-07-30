45d67d9a5b784d8e4e7e3eb8e858e1eafc1a23b4
Linga Raja <lingaraja@neemtecsolutions.com>
2015-07-30 16:00:06 +0530

    Added form validation in post and category

--- server/publications/categoryPub.js
+++ server/publications/categoryPub.js
@@ -11,16 +11,16 @@ Meteor.publish('Category', function () {
 
 //Define all the methods interact with the POSTS object
 Meteor.methods({
-    'addNewCategory': function (title, content, slug, parentCategory) {
-        Category.insert({name: title,description: content,slug: slug,parent: parentCategory});
+    'addNewCategory': function (title, content, parentCategory) {
+        Category.insert({name: title,description: content,parent: parentCategory});
         console.log('category '+ title + ' added successfully');
     },
     'removeCategory' : function(id) {
         Category.remove({_id: id});
         console.log('Category removed successfully');      	
     },
-    'updateCategory' : function(id, title, content, slug, parentCategory) {
-        Category.update(id, {$set: {name: title,description: content,slug: slug,parent: parentCategory}});
+    'updateCategory' : function(id, title, content, parentCategory) {
+        Category.update(id, {$set: {name: title,description: content,parent: parentCategory}});
         console.log('category '+ title + ' updated successfully');    	
     },
    'searchCategory' : function(queryString) {
