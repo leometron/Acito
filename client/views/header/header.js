65e22763e14e992ce8b64e1828e3882a56ccf29f
satheeshraj <satheeshraj@ymail.com>
2015-08-05 18:22:27 +0530

    UI Changes

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -54,11 +54,15 @@ $(window).scroll(function(){
    var window_top = $(window).scrollTop();
    if(window_top != 0){     
        $('#mainNav').css({'background-color': 'white'});  
-       $('.navbar-brand').css({'font-size': '15px'});          
+       $('.navbar-brand').css({'font-size': '15px'});  
+       $('.navbar-default .navbar-nav li a').css({'color':'black'})  
+       $('.navbar-default .navbar-brand').css({'color':'black'})        
    }else{
        $('#mainNav').scrollTop(function(){           
-           $(this).css({'background-color': 'transparent','border-color':'white'})
-            $('.navbar-brand').css({'font-size': 'x-large'});  
+       $(this).css({'background-color': 'transparent','border-color':'white'})
+       $('.navbar-brand').css({'font-size': 'x-large'}); 
+       $('.navbar-default .navbar-nav li a').css({'color':'white'})   
+       $('.navbar-default .navbar-brand').css({'color':'white'})  
        })
    }
 });
