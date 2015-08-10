fad428238d4309c171a9ea2cda59ed8f640e40a1
Linga Raja <lingaraja@neemtecsolutions.com>
2015-08-10 16:15:42 +0530

    Initiate work for theme

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,9 +1,23 @@
 // Home Route
+var themeName;
 Router.route('/', {
   layoutTemplate: 'basicLayout',	
   name: 'home',
+  data: function() {          
+       // var themeObj =  theme.findOne({userId: Meteor.userId()});
+       // if (themeObj) {
+       //  themeName = themeObj.themeName;
+       // console.log('themeObj..............'+themeObj.themeName);
+       //         this.render(themeObj.themeName);        
+       // } else {
+        this.render('home');        
+       // }
+    },
   action: function () {
-    this.render('home');
+    // var themeObj = theme.findOne({userId: Meteor.userId()});
+    //   console.log('themeobject............'+Meteor.userId());
+          // console.log('themename............'+data);
+        // this.render('home');
     SEO.set({ title: 'Home - ' + Meteor.App.NAME });
     console.log(Meteor.App.NAME);
   }
@@ -16,6 +30,7 @@ Router.route('/post/:_id', {
   name: 'postDetail',
   action: function () {
     // this.render('home');
+    console.log('user id............'+Meteor.userId());    
     this.render('postDetail');    
     SEO.set({ title: 'postdetails - ' + Meteor.App.NAME });
     console.log(Meteor.App.NAME);
