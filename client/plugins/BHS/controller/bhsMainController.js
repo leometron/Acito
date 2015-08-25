f2122dd4bdc4e38e1806d439a40d50123360b5b7
Linga Raja <lingaraja@neemtecsolutions.com>
2015-08-25 17:28:30 +0530

    Completed the UI issue in list screen

--- client/plugins/BHS/controller/bhsMainController.js
+++ client/plugins/BHS/controller/bhsMainController.js
@@ -23,7 +23,8 @@ Template.BHSlist.helpers({
 	},
 	'icdSectionAlphabet':function(){
 		var alphabetArray = new Array();
-		var data = section.find({'type':'ICD'}).fetch();
+		var type = (Session.get('title') == "ICD-10 codes") ? "ICD" : "DSM";
+		var data = section.find({'type':type}).fetch();
 		if(data.length>0){
 			$.each(data, function(i,row) {
     			var x = row.sectionName.charAt(0).toUpperCase();
@@ -36,17 +37,17 @@ Template.BHSlist.helpers({
 	},
 
 	'listIcd' : function(){
-		if(Session.get('title') == "ICD-10-CM") {
+		if(Session.get('title') == "ICD-10 codes") {
 			return ICD.find();
 		}
 	},
 	'listDSM' : function(){
-		if(Session.get('title') == "DSM-5-CM") {
+		if(Session.get('title') == "DSM-5 codes") {
 			return DSM.find();
 		}
 	},
 	'listCodingRule' : function(){
-		if(Session.get('title') == "Coding Rules") {
+		if(Session.get('title') == "ICD-10 CM Coding and Documentation Rules") {
 			return codingRules.find();
 		}
 	},
@@ -111,7 +112,7 @@ Template.BHSlist.events({
   	},
   	'keydown #searchString' : function(e){
   		if (e.which == 13) {
-			if(Session.get('title') == "Coding Rules") {
+			if(Session.get('title') == "ICD-10 CM Coding and Documentation Rules") {
 				Meteor.call('searchCodingRules',$('#searchString').val());
 			} else {
   				Meteor.call('showSearchSection',$('#searchString').val());			
