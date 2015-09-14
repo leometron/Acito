c570e0bcd9a1806c1c6871896bc628b5c5125bb7
Linga Raja <lingaraja@neemtecsolutions.com>
2015-09-14 17:51:18 +0530

    Added client side question details functionalities

--- server/publications/questionDetailPub.js
+++ server/publications/questionDetailPub.js
@@ -1,3 +1,10 @@
 Meteor.publish('questionDetail', function () {
   return questionDetail.find();
 });
+
+Meteor.methods({
+    'postQuestion': function (question, details, majorComplaint, detailExplanation, complaintPeriod, symptoms, secondaryComplaint, medicineTaking) {
+        questionDetail.insert({question: question, detail: details,complaint: majorComplaint, complaintDetail: detailExplanation,complaintPeriod: complaintPeriod, symptoms: symptoms,secondaryComplaint: secondaryComplaint, medicine: medicineTaking});
+        console.log('question posted successfully');     
+    }
+});
